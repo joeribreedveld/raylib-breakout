5f8e06d97a2bd38bc84b97d02823845b081bc1a9
joeribreedveld <joeri.breedveld123@gmail.com>
2025-07-03 08:14:00 +0200

    build: setup cmake and raylib

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main() {
-    printf("Hello, World!\n");
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Breakout");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
